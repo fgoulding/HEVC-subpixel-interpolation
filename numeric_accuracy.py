c5da84013c6016fc10c4b593e3be9f9944761599
fatemaemad <falmeshq@andrew.cmu.edu>
2018-12-03 08:46:57 -0500

    visualization for large images

--- numeric_accuracy.py
+++ numeric_accuracy.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-# from skimage.measure import compare_ssim as ssim
+from skimage.measure import compare_ssim as ssim
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,10 +15,11 @@ def process_output_array(filename):
     return output_array;
 
 def construct_image(subpixels, integer_pixels):
-    dim = len(integer_pixels);
-    image = np.zeros((dim*4, dim*4));
-    for i in xrange(dim):
-        for j in xrange(dim):
+    dim_x = len(integer_pixels)#.shape[0];
+    dim_y = len(integer_pixels[0])#.shape[1];
+    image = np.zeros((dim_x*4, dim_y*4));
+    for i in xrange(dim_x):
+        for j in xrange(dim_y):
             I = i*4;
             J = j*4;
             image[I][J] = integer_pixels[i][j];
@@ -32,22 +33,75 @@ def construct_image(subpixels, integer_pixels):
             image[I+2][J] = subpixels[1][i+8][j];
             image[I+3][J] = subpixels[2][i+8][j];
 
-            # image[I+1][J+1] = subpixels[0][i+16][j];
-            # image[I+2][J+1] = subpixels[1][i+16][j];
-            # image[I+3][J+1] = subpixels[2][i+16][j];
-            #
-            # image[I+1][J+2] = subpixels[0][i+24][j];
-            # image[I+2][J+2] = subpixels[1][i+24][j];
-            # image[I+3][J+2] = subpixels[2][i+24][j];
-            #
-            # image[I+1][J+1] = subpixels[0][i+32][j];
-            # image[I+2][J+1] = subpixels[1][i+32][j];
-            # image[I+3][J+1] = subpixels[2][i+32][j];
-
-    print count
+            image[I+1][J+1] = subpixels[0][i+16][j];
+            image[I+2][J+1] = subpixels[1][i+16][j];
+            image[I+3][J+1] = subpixels[2][i+16][j];
+            
+            image[I+1][J+2] = subpixels[0][i+24][j];
+            image[I+2][J+2] = subpixels[1][i+24][j];
+            image[I+3][J+2] = subpixels[2][i+24][j];
+            
+            image[I+1][J+3] = subpixels[0][i+32][j];
+            image[I+2][J+3] = subpixels[1][i+32][j];
+            image[I+3][J+3] = subpixels[2][i+32][j];
 
     return image;
 
+
+def construct_image_large(subpixels, integer_pixels):
+    block_size = 8;
+    num_entries = 40;
+    decompress_rate = 4;
+    dim_x = len(integer_pixels)
+    dim_y = len(integer_pixels[0])
+    num_sub_images = len(subpixels[0])/num_entries;  
+    num_col_blocks = dim_y/block_size;
+    num_row_blocks = dim_x/block_size; 
+    image = np.zeros((dim_x*decompress_rate, dim_y*decompress_rate)); #should be 96x64
+    #print image.shape;
+
+    for step_x in xrange(num_col_blocks):
+        col_jump = step_x*block_size;
+
+        for step_y in xrange(num_row_blocks):
+            input_jump = step_y*num_entries + step_x*(num_row_blocks)*num_entries;
+            row_jump = step_y*block_size;
+
+            for i in xrange(block_size):
+                I = (i + row_jump)*decompress_rate;
+                half_horz_i = i+input_jump;
+                half_vert_i = half_horz_i + 8;
+                quarter_1_i = half_horz_i + 16;
+                quarter_2_i = half_horz_i + 24;
+                quarter_3_i = half_horz_i + 32;
+
+                for j in xrange(block_size):
+                    J = (j + col_jump)*decompress_rate;
+                    #print row_jump, I, "-----", col_jump, J#, half_horz_i
+
+                    image[I][J] = integer_pixels[i+row_jump][j+col_jump];
+                    
+                    image[I][J+1] = subpixels[0][half_horz_i][j];
+                    image[I][J+2] = subpixels[1][half_horz_i][j];
+                    image[I][J+3] = subpixels[2][half_horz_i][j];
+                    
+                    image[I+1][J] = subpixels[0][half_vert_i][j];
+                    image[I+2][J] = subpixels[1][half_vert_i][j];
+                    image[I+3][J] = subpixels[2][half_vert_i][j];
+
+                    image[I+1][J+1] = subpixels[0][quarter_1_i][j];
+                    image[I+2][J+1] = subpixels[1][quarter_1_i][j];
+                    image[I+3][J+1] = subpixels[2][quarter_1_i][j];
+                    
+                    image[I+1][J+2] = subpixels[0][quarter_2_i][j];
+                    image[I+2][J+2] = subpixels[1][quarter_2_i][j];
+                    image[I+3][J+2] = subpixels[2][quarter_2_i][j];
+                    
+                    image[I+1][J+3] = subpixels[0][quarter_3_i][j];
+                    image[I+2][J+3] = subpixels[1][quarter_3_i][j];
+                    image[I+3][J+3] = subpixels[2][quarter_3_i][j];
+
+    return image;
 
 def construct_bigger_ogimage(integer_pixels):
     dim = len(integer_pixels);
@@ -67,17 +121,17 @@ def construct_bigger_ogimage(integer_pixels):
             image[I+2][J] = integer_pixels[i][j];
             image[I+3][J] = integer_pixels[i][j];
             #
-            # image[I+1][J+1] = integer_pixels[i][j]
-            # image[I+1][J+2] = integer_pixels[i][j]
-            # image[I+1][J+3] = integer_pixels[i][j]
-            #
-            # image[I+2][J+1] = integer_pixels[i][j]
-            # image[I+2][J+2] = integer_pixels[i][j]
-            # image[I+2][J+3] = integer_pixels[i][j]
-            #
-            # image[I+3][J+1] = integer_pixels[i][j]
-            # image[I+3][J+2] = integer_pixels[i][j]
-            # image[I+3][J+3] = integer_pixels[i][j]
+            image[I+1][J+1] = integer_pixels[i][j]
+            image[I+1][J+2] = integer_pixels[i][j]
+            image[I+1][J+3] = integer_pixels[i][j]
+            
+            image[I+2][J+1] = integer_pixels[i][j]
+            image[I+2][J+2] = integer_pixels[i][j]
+            image[I+2][J+3] = integer_pixels[i][j]
+            
+            image[I+3][J+1] = integer_pixels[i][j]
+            image[I+3][J+2] = integer_pixels[i][j]
+            image[I+3][J+3] = integer_pixels[i][j]
 
     return image;
 
@@ -113,64 +167,91 @@ def construct_frame(subpixels, integer_pixels):
     return image_array;
 
 def main():
-    np.set_printoptions(threshold=np.nan)
+    #np.set_printoptions(threshold=np.nan)
 
     image_array = []
+    image_array_large = []
     with open("image_array/test_image_4.mem") as file:
         for line in file.readlines():
             data = line.strip().split()
-            image_array.append([int(i,16) for i in data][3:len(data)-4])
+            entry = [int(i,16) for i in data][3:len(data)-4]
+            image_array.append(entry)
+            image_array_large.append(entry + entry)
+            image_array_large.append(entry + entry)
 
-    image_array = np.array(image_array)
-    length = len(image_array);
-
-    # original image without interpolation
-    out_og = Image.fromarray(image_array[3:length-4])
-    plt.imshow(out_og)
-    plt.xlabel("original image (no interpolation)");
 
     # regular interpolation without optimization
-    A = process_output_array("output/output_4_a.txt");
-    B = process_output_array("output/output_4_b.txt");
-    C = process_output_array("output/output_4_c.txt");
+    A_large = process_output_array("output/output_4_16x24.txt");
+    B_large = process_output_array("output/output_4_16x24.txt");
+    C_large = process_output_array("output/output_4_16x24.txt");
 
-    out_verilog = [A, B, C];
-    large_og = construct_bigger_ogimage(image_array[3:11]);
-    out_og = Image.fromarray(large_og)
-    out_og.show()
+    out_verilog_l = [A_large, B_large, C_large];
+    image_large = construct_image_large(out_verilog_l, image_array_large[6:len(image_array_large)-8]);
+    pprint(image_large)
+    out_large = Image.fromarray((image_large * 255).astype(np.uint8))
+    plt.figure(1)
+    plt.imshow(out_large)
+    plt.xlabel("actual interpolated image large!");
 
-    image_actual = construct_image(out_verilog, image_array[3:11]);
-    out_actual = Image.fromarray((image_actual).astype(np.uint8))
-    plt.figure(2)
-    plt.imshow(out_actual)
-    plt.xlabel("actual interpolated image");
+    # image_array = np.array(image_array)
+    # length = len(image_array);
 
-    # loop perforation
-    A_loop = process_output_array("output/output_4_a_loop.txt");
-    B_loop = process_output_array("output/output_4_b_loop.txt");
-    C_loop = process_output_array("output/output_4_c_loop.txt");
+    # # original image without interpolation
+    # out_og = Image.fromarray(image_array[3:length-4])
+    # plt.figure(2)
+    # plt.imshow(out_og)
+    # plt.xlabel("original image (no interpolation)");
 
-    out_loop = [A_loop, B_loop, C_loop]
-    image_pred = construct_image(out_loop, image_array[3:11]);
-    out_pred = Image.fromarray((image_pred).astype(np.uint8))
-    plt.figure(3);
-    plt.imshow(out_pred)
-    plt.xlabel("approximately interpolated image (loop perforation)");
+    # # regular interpolation without optimization
+    # A = process_output_array("output/output_4_a.txt");
+    # B = process_output_array("output/output_4_b.txt");
+    # C = process_output_array("output/output_4_c.txt");
 
-    # approximate multiplierless
-    A_multiplierless = process_output_array("output/output_4_a_approx_multiplierless.txt");
-    B_multiplierless = process_output_array("output/output_4_b_approx_multiplierless.txt");
-    C_multiplierless = process_output_array("output/output_4_c_approx_multiplierless.txt");
+    # out_verilog = [A, B, C];
+    # image_actual = construct_image(out_verilog, image_array[3:length-4]);
+    # out_actual = Image.fromarray((image_actual).astype(np.uint8))
+    # plt.figure(3)
+    # plt.imshow(out_actual)
+    # plt.xlabel("actual interpolated image");
 
-    out_multiplierless = [A_multiplierless, B_multiplierless, C_multiplierless]
-    image_pred_2 = construct_image(out_multiplierless, image_array[3:11]);
-    out_pred_2 = Image.fromarray((image_pred_2).astype(np.uint8))
-    plt.figure(4);
-    plt.imshow(out_pred_2)
-    plt.xlabel("approximately interpolated image (approx multiplierless)");
+    # # loop perforation
+    # A_loop = process_output_array("output/output_4_a_loop.txt");
+    # B_loop = process_output_array("output/output_4_b_loop.txt");
+    # C_loop = process_output_array("output/output_4_c_loop.txt");
 
-    #pprint(image_actual)
-    #pprint(image_pred)
+    # out_loop = [A_loop, B_loop, C_loop]
+    # image_pred = construct_image(out_loop, image_array[3:length-4]);
+    # out_pred = Image.fromarray((image_pred).astype(np.uint8))
+    # plt.figure(4);
+    # plt.imshow(out_pred)
+    # plt.xlabel("approximately interpolated image (loop perforation)");
+
+    # # approximate multiplierless
+    # A_multiplierless = process_output_array("output/output_4_a_approx_multiplierless.txt");
+    # B_multiplierless = process_output_array("output/output_4_b_approx_multiplierless.txt");
+    # C_multiplierless = process_output_array("output/output_4_c_approx_multiplierless.txt");
+
+    # out_multiplierless = [A_multiplierless, B_multiplierless, C_multiplierless]
+    # image_pred_2 = construct_image(out_multiplierless, image_array[3:length-4]);
+    # out_pred_2 = Image.fromarray((image_pred_2).astype(np.uint8))
+    # plt.figure(5);
+    # plt.imshow(out_pred_2)
+    # plt.xlabel("approximately interpolated image (approx multiplierless)");
+
+    # #pprint(image_actual)
+    # #pprint(image_pred)
+
+    # # approximate multiplierless + loop perforation (hybrid)
+    # A_hybrid = process_output_array("output/output_4_a_hybrid.txt");
+    # B_hybrid = process_output_array("output/output_4_b_hybrid.txt");
+    # C_hybrid = process_output_array("output/output_4_c_hybrid.txt");
+
+    # out_hybrid = [A_hybrid, B_hybrid, C_hybrid]
+    # image_pred_3 = construct_image(out_hybrid, image_array[3:length-4]);
+    # out_pred_3 = Image.fromarray((image_pred_3).astype(np.uint8))
+    # plt.figure(6);
+    # plt.imshow(out_pred_3)
+    # plt.xlabel("approximately interpolated image (hybrid)");
 
     # regular interpolation without optimization as frames
     # image_frame = construct_frame(out_verilog, image_array[3:11]);
@@ -185,30 +266,39 @@ def main():
     plt.show()
 
     # calculate for accuracy
-    mse_loop = 0;
-    mse_mult = 0;
-    for i in xrange(len(out_verilog)):
-        print "Testing for output_" + chr(i + 65)
-        actual = np.matrix(out_verilog[i]);
-        pred_1 = np.matrix(out_loop[i])
-        print "MSE (loop):  " , mean_squared_error(actual, pred_1)
-        mse_loop += mean_squared_error(actual, pred_1)
-        pred_2 = np.matrix(out_multiplierless[i])
-        print "MSE (multiplierless):  " , mean_squared_error(actual, pred_2)
-        mse_mult += mean_squared_error(actual, pred_2)
+    # mse_loop = 0;
+    # mse_mult = 0;
+    # mse_hybrid = 0;
+    # for i in xrange(len(out_verilog)):
+    #     print "Testing for output_" + chr(i + 65)
+    #     actual = np.matrix(out_verilog[i]);
+    #     pred_1 = np.matrix(out_loop[i])
+    #     print "MSE (loop):  " , mean_squared_error(actual, pred_1)
+    #     mse_loop += mean_squared_error(actual, pred_1)
+    #     pred_2 = np.matrix(out_multiplierless[i])
+    #     print "MSE (multiplierless):  " , mean_squared_error(actual, pred_2)
+    #     mse_mult += mean_squared_error(actual, pred_2)
+    #     pred_3 = np.matrix(out_hybrid[i])
+    #     print "MSE (multiplierless):  " , mean_squared_error(actual, pred_3)
+    #     mse_hybrid += mean_squared_error(actual, pred_3)
 
-    s_loop = ssim(image_actual, image_pred);
-    s_mult = ssim(image_actual, image_pred_2);
+    # s_loop = ssim(image_actual, image_pred);
+    # s_mult = ssim(image_actual, image_pred_2);
+    # s_hybrid = ssim(image_actual, image_pred_3);
 
-    print
-    print "Full image results (loop)"
-    print "ssim accuracy measure (-1 to 1): " , s_loop
-    print "mse accuracy measure (unbounded): ", mse_loop
-    print
-    print "Full image results (multiplierless)"
-    print "ssim accuracy measure (-1 to 1): " , s_mult
-    print "mse accuracy measure (unbounded): ", mse_mult
-    print
+    # print
+    # print "Full image results (loop)"
+    # print "ssim accuracy measure (-1 to 1): " , s_loop
+    # print "mse accuracy measure (unbounded): ", mse_loop
+    # print
+    # print "Full image results (multiplierless)"
+    # print "ssim accuracy measure (-1 to 1): " , s_mult
+    # print "mse accuracy measure (unbounded): ", mse_mult
+    # print
+    # print "Full image results (hybrid)"
+    # print "ssim accuracy measure (-1 to 1): " , s_hybrid
+    # print "mse accuracy measure (unbounded): ", mse_hybrid
+    # print
 
 if __name__ == '__main__':
     main()
---
+++ tb_v_hybrid_real.v
@@ -0,0 +1,188 @@
+`include "sub_pixel_hybrid_real.v"
+
+module tb;
+  reg clk, reset;
+  reg [55:0] inputPixels;
+  reg [7:0] im_memory [0:14][0:14];
+  reg [119:0] im_rows [0:14];
+  wire [119:0] input_row;
+  reg [1799:0] integer_array;
+  wire [2559:0] A;
+  wire [2559:0] B;
+  wire [2559:0] C;
+  wire [63:0] fir_out_a;
+  wire [63:0] fir_out_b;
+  wire [63:0] fir_out_c;
+  wire [959:0] temp_A;
+  wire [959:0] temp_B;
+  wire [959:0] temp_C;
+  wire [7:0] cnt;
+  wire load_out;
+  wire [7:0] sel;
+  wire [119:0] currentPixels;
+  wire [63:0] next_row;
+
+  subpixel_interpolation dut(
+    .clk(clk),
+    .rst(reset),
+    .in_row(input_row),
+    .next_row(next_row),
+    .out_A(A),
+    .out_B(B),
+    .out_C(C),
+    .cnt(cnt),
+    .fir_out_a(fir_out_a),
+    .fir_out_b(fir_out_b),
+    .fir_out_c(fir_out_c),
+    .temp_B(temp_B),
+    .load_out(load_out),
+    .sel(sel),
+    .currentPixels(currentPixels)
+    );
+
+  assign input_row = im_rows[next_row];
+
+  integer i;
+  integer j;
+  integer f_a;
+  integer f_b;
+  integer f_c;
+initial begin
+  f_a = $fopen("output/output_4_a_hybrid.txt");
+  f_b = $fopen("output/output_4_b_hybrid.txt");
+  f_c = $fopen("output/output_4_c_hybrid.txt");
+  $monitor({"clk = %b; next_row = %d;  reset:%h cnt:%h loadOut:%h sel:%h ---\n",
+  "---ROW---\n%h\n------\n",
+  "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n------\n",
+  "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n------\n",
+   "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n------\n",
+   "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n------\n",
+   "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n------\n\n"},
+  clk,next_row,reset,cnt, load_out, sel, input_row,fir_out_b,
+  C[63:0],
+  C[127:64],
+  C[191:128],
+  C[255:192],
+  C[319:256],
+  C[383:320],
+  C[447:384],
+  C[511:448],
+  C[575:512],
+  C[639:576],
+  C[703:640],
+  C[767:704],
+  C[831:768],
+  C[895:832],
+  C[959:896],
+  C[1023:960],
+  C[1087:1024],
+  C[1151:1088],
+  C[1215:1152],
+  C[1279:1216],
+  C[1343:1280],
+  C[1407:1344],
+  C[1471:1408],
+  C[1535:1472],
+  C[1599:1536],
+  C[1663:1600],
+  C[1727:1664],
+  C[1791:1728],
+  C[1855:1792],
+  C[1919:1856],
+  C[1983:1920],
+  C[2047:1984],
+  C[2111:2048],
+  C[2175:2112],
+  C[2239:2176],
+  C[2303:2240],
+  C[2367:2304],
+  C[2431:2368],
+  C[2495:2432],
+  C[2559:2496]);
+  // $monitor({"cnt:%h---\n currentPixels: %h firA: \n %h %h %h %h %h %h %h %h\n ------\n",
+  // "%h\n"},
+  // cnt, currentPixels,
+  // fir_out_a[7:0],fir_out_a[15:8],fir_out_a[23:16],fir_out_a[31:24],
+  // fir_out_a[39:32],fir_out_a[47:40], fir_out_a[55:48], fir_out_a[63:56],
+  // temp_A);
+end
+
+initial begin
+  $write("Loading rom...");
+  $readmemh("image_array/test_image_4.mem", im_memory);
+
+  for (i=0; i<15; i=i+1) begin
+      im_rows[i] = {im_memory[i][14],im_memory[i][13],im_memory[i][12],im_memory[i][11],im_memory[i][10],
+                    im_memory[i][9],im_memory[i][8],im_memory[i][7],im_memory[i][6],im_memory[i][5],
+                    im_memory[i][4],im_memory[i][3],im_memory[i][2],im_memory[i][1],im_memory[i][0]};
+  end
+  $display("Done");
+  clk = 0;
+  reset = 0;
+  #130;
+  reset = 1;
+
+end
+
+
+always
+  #120 clk = !clk;
+
+
+  initial begin
+    #13500 $fwrite(f_c, {"%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+      "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+      "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+      "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+      "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n"},
+       C[63:0], C[127:64], C[191:128], C[255:192],
+       C[319:256], C[383:320], C[447:384], C[511:448],
+       C[63:0], C[127:64], C[191:128], C[255:192],
+       C[319:256], C[383:320], C[447:384], C[511:448],
+       C[575:512], C[639:576], C[703:640], C[767:704],
+       C[831:768], C[895:832], C[959:896], C[1023:960],
+       C[575:512], C[639:576], C[703:640], C[767:704],
+       C[831:768], C[895:832], C[959:896], C[1023:960],
+       C[1087:1024], C[1151:1088], C[1215:1152], C[1279:1216],
+       C[1343:1280], C[1407:1344], C[1471:1408], C[1535:1472]);
+  $fwrite(f_b, {"%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n"},
+       B[63:0], B[127:64], B[191:128], B[255:192],
+       B[319:256], B[383:320], B[447:384], B[511:448],
+       B[63:0], B[127:64], B[191:128], B[255:192],
+       B[319:256], B[383:320], B[447:384], B[511:448],
+       B[575:512], B[639:576], B[703:640], B[767:704],
+       B[831:768], B[895:832], B[959:896], B[1023:960],
+       B[575:512], B[639:576], B[703:640], B[767:704],
+       B[831:768], B[895:832], B[959:896], B[1023:960],
+       B[1087:1024], B[1151:1088], B[1215:1152], B[1279:1216],
+       B[1343:1280], B[1407:1344], B[1471:1408], B[1535:1472]);
+  $fwrite(f_a, {"%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
+    "%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n"},
+       A[63:0], A[127:64], A[191:128], A[255:192],
+       A[319:256], A[383:320], A[447:384], A[511:448],
+       A[63:0], A[127:64], A[191:128], A[255:192],
+       A[319:256], A[383:320], A[447:384], A[511:448],
+       A[575:512], A[639:576], A[703:640], A[767:704],
+       A[831:768], A[895:832], A[959:896], A[1023:960],
+       A[575:512], A[639:576], A[703:640], A[767:704],
+       A[831:768], A[895:832], A[959:896], A[1023:960],
+       A[1087:1024], A[1151:1088], A[1215:1152], A[1279:1216],
+       A[1343:1280], A[1407:1344], A[1471:1408], A[1535:1472]);
+    $fclose(f_c);
+    $fclose(f_b);
+    $fclose(f_a);
+    $finish;
+
+  end
+
+
+
+
+endmodule
